e7d15685ac20ca77fd49396c8b63adf9347c3da2
Chen Honggang <chenhg2@lenovo.com>
2024-04-19 19:14:53 +0800

    增加raid1 raid1_submit_null_payload_request

--- module/bdev/raid/raid1.c
+++ module/bdev/raid/raid1.c
@@ -372,6 +372,107 @@ raid1_submit_process_request(struct raid_bdev_process_request *process_req,
 	}
 }
 
+static void
+raid1_base_io_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
+{
+    struct raid_bdev_io *raid_io = cb_arg;
+
+    if (!success) {
+        /* If any base device I/O operation fails, mark the RAID I/O operation as failed. */
+        raid_io->status = SPDK_BDEV_IO_STATUS_FAILED;
+    }
+
+    raid_io->base_bdev_io_remaining--;
+
+    if (raid_io->base_bdev_io_remaining == 0) {
+        /* All base device I/O operations have completed. */
+        raid_bdev_io_complete(raid_io, raid_io->status);
+    }
+
+    spdk_bdev_free_io(bdev_io);
+}
+
+static inline void
+_raid1_get_io_range(struct raid_bdev_io_range *io_range,
+            uint8_t num_base_bdevs, uint64_t strip_size, uint64_t strip_size_shift,
+            uint64_t offset_blocks, uint64_t num_blocks)
+{
+    uint64_t	total_blocks;
+
+    io_range->strip_size = strip_size;
+    total_blocks = offset_blocks + num_blocks;
+
+    /* The start and end block index in raid1 bdev scope */
+    io_range->start_offset_in_strip = offset_blocks;
+    io_range->end_offset_in_strip = total_blocks;
+
+    /* The base bdev indexes in which start and end blocks are located */
+    io_range->start_disk = 0;
+    io_range->end_disk = num_base_bdevs - 1;
+
+    /* Calculate how many base_bdevs are involved in io operation.
+     * In RAID 1, all base bdevs are involved.
+     */
+    io_range->n_disks_involved = num_base_bdevs;
+}
+
+void raid1_submit_null_payload_request(struct raid_bdev_io *raid_io)
+{
+    struct raid_bdev		*raid_bdev;
+    struct raid_bdev_io_range	io_range;
+    int				ret;
+    struct raid_base_bdev_info	*base_info;
+    struct spdk_io_channel		*base_ch;
+
+    raid_bdev = raid_io->raid_bdev;
+
+    _raid1_get_io_range(&io_range, raid_bdev->num_base_bdevs,
+                raid_bdev->strip_size, raid_bdev->strip_size_shift,
+                raid_io->offset_blocks, raid_io->num_blocks);
+
+    if (raid_io->base_bdev_io_remaining == 0) {
+        raid_io->base_bdev_io_remaining = io_range.n_disks_involved;
+    }
+
+    for (uint8_t disk_idx = 0; disk_idx < raid_bdev->num_base_bdevs; disk_idx++) {
+        base_info = &raid_bdev->base_bdev_info[disk_idx];
+        base_ch = raid_bdev_channel_get_base_channel(raid_io->raid_ch, disk_idx);
+
+        switch (raid_io->type) {
+        case SPDK_BDEV_IO_TYPE_UNMAP:
+            ret = raid_bdev_unmap_blocks(base_info, base_ch,
+                             raid_io->offset_blocks, raid_io->num_blocks,
+                             raid1_base_io_complete, raid_io);
+            break;
+
+        case SPDK_BDEV_IO_TYPE_FLUSH:
+            ret = raid_bdev_flush_blocks(base_info, base_ch,
+                             raid_io->offset_blocks, raid_io->num_blocks,
+                             raid1_base_io_complete, raid_io);
+            break;
+
+        default:
+            SPDK_ERRLOG("submit request, invalid io type with null payload %u\n", raid_io->type);
+            assert(false);
+            ret = -EIO;
+        }
+
+        if (ret == 0) {
+            raid_io->base_bdev_io_submitted++;
+        } else if (ret == -ENOMEM) {
+            raid_bdev_queue_io_wait(raid_io, spdk_bdev_desc_get_bdev(base_info->desc),
+                        base_ch, _raid1_submit_null_payload_request);
+            return;
+        } else {
+            SPDK_ERRLOG("bdev io submit error not due to ENOMEM, it should not happen\n");
+            assert(false);
+            raid_bdev_io_complete(raid_io, SPDK_BDEV_IO_STATUS_FAILED);
+            return;
+        }
+    }
+}
+
+
 static struct raid_bdev_module g_raid1_module = {
 	.level = RAID1,
 	.base_bdevs_min = 2,
@@ -382,6 +483,7 @@ static struct raid_bdev_module g_raid1_module = {
 	.submit_rw_request = raid1_submit_rw_request,
 	.get_io_channel = raid1_get_io_channel,
 	.submit_process_request = raid1_submit_process_request,
+	.submit_null_payload_request = raid1_submit_null_payload_request,
 };
 RAID_MODULE_REGISTER(&g_raid1_module)
 
